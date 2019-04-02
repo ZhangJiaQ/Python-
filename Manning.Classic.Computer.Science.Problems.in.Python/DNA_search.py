241e0e37ec24298fc37235533e3bf45c10e7282e
ZhangJiaQ <529326573@qq.com>
2019-04-02 21:49:46 +0800

    继续写题

--- Manning.Classic.Computer.Science.Problems.in.Python/DNA_search.py
+++ Manning.Classic.Computer.Science.Problems.in.Python/DNA_search.py
@@ -33,7 +33,17 @@ def linear_contains(gene: Gene, key_codon: Codon) -> bool:
 
 
 def binary_contains(gene: Gene, key_codon: Codon) ->bool:
-    pass
+    low: int = 0
+    high: int = len(gene) - 1
+    while low <= high:
+        mid: int = (low + high) // 2
+        if gene[mid] < key_codon:
+            low = mid + 1
+        elif gene[mid] > key_codon:
+            high = mid - 1
+        else:
+            return True
+        return False
 
 
 if __name__ == "__main__":
