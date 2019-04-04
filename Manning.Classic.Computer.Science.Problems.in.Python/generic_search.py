7116bb68c7f8db2ed3d7ddcf6bf2c0e8fe7c48da
ZhangJiaQ <529326573@qq.com>
2019-04-04 18:00:28 +0800

    learing

--- Manning.Classic.Computer.Science.Problems.in.Python/generic_search.py
+++ Manning.Classic.Computer.Science.Problems.in.Python/generic_search.py
@@ -2,6 +2,32 @@
 # @Time    : 2019-04-02 21:58
 # @Author  : 张嘉麒
 # @File    : generic_search.py
+from typing import Generic, List, TypeVar
+
+T = TypeVar('T')
+
+
+class Stack(Generic[T]):
+
+    def __init__(self) -> None:
+        self._container: List[T] = []
+
+    def pop(self) -> T:
+        return self._container.pop()
+
+    def push(self, item: T) -> None:
+        self._container.append(item)
+
+    def __repr__(self) -> str:
+        return repr(self._container)
+
+    def empty(self) -> bool:
+        return not self._container
+
+class Node(Generic[T]):
+    def __init__(self):
+        pass
+
 
 def dfs():
     return None
@@ -13,7 +39,4 @@ def node_to_path():
     return None
 
 def astar():
-    return None
-
-def Node():
     return None
